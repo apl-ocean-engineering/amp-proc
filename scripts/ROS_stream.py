36487d27bfc0eb1287c5645fb821e5d281ce5673
Mitchell Scott <miscott@uw.edu>
2019-09-25 10:55:52 -0700

    Adding ROS image/info stream. Updating YOLO stream for proper rectification and undistortion despite high distortion values

---
+++ scripts/ROS_stream.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+from ampProc.amp_common import ampCommon
+import cv2
+import glob
+import yaml
+from os.path import dirname, abspath
+from ampProc.intrinsic_extrinsic import Loader, ExtrinsicIntrnsicLoaderSaver
+import argparse
+import rospy
+from sensor_msgs.msg import CameraInfo, Image
+import std_msgs
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+img1_pub = rospy.Publisher("manta/left/image_raw", Image, queue_size=10)
+img2_pub = rospy.Publisher("manta/right/image_raw", Image, queue_size=10)
+
+info1_pub = rospy.Publisher("manta/left/camera_info", CameraInfo, queue_size=10)
+info2_pub = rospy.Publisher("manta/right/camera_info", CameraInfo, queue_size=10)
+
+bridge = CvBridge()
+
+
+def get_header(camera):
+    header = std_msgs.msg.Header()
+    header.stamp = rospy.Time.now()
+
+    if camera == 1:
+        header.frame_id = "manta1"
+    elif camera == 2:
+        header.frame_id = "manta2"
+
+    return header
+
+
+def construct_info_message(im_size, K, D, R, P):
+    camera_info_msg = CameraInfo()
+    camera_info_msg.width = im_size[0]
+    camera_info_msg.height = im_size[1]
+    #print(K)
+    camera_info_msg.K = list(K.reshape((1,9)))[0]
+    camera_info_msg.D = list(D.reshape((1,5)))[0]
+    camera_info_msg.R = list(R.reshape((1,9)))[0]
+    camera_info_msg.P = list(P.reshape((1,12)))[0]
+    camera_info_msg.distortion_model = "plumb_bob"
+
+    return camera_info_msg
+
+
+def main(args):
+    with open(args.calibration_yaml, 'r') as stream:
+        calibration_loader = yaml.safe_load(stream)
+
+    loader = Loader()
+    loader.load_params_from_file(calibration_loader)
+
+    manta1 = sorted(glob.glob(args.images + "/Manta1/*.jpg"))
+    manta2 = sorted(glob.glob(args.images + "/Manta2/*.jpg"))
+
+    images = []
+    for fname1, fname2 in zip(manta1, manta2):
+        images.append((fname1, fname2))
+    ac = ampCommon()
+    ac.display = False
+
+    img1 = cv2.imread(images[0][0])
+    im_size = img1.shape[:2]
+    EI_loader = ExtrinsicIntrnsicLoaderSaver(loader, im_size)
+    # # Get rectification maps
+    if EI_loader.paramaters.R1 is None or EI_loader.paramaters.R1 is None:
+        EI_loader.calculate_rectification_matracies()
+
+    new_k2, roi = cv2.getOptimalNewCameraMatrix(
+        EI_loader.paramaters.K2, EI_loader.paramaters.d2, im_size, 1, im_size)
+
+    map1, map2 = cv2.initUndistortRectifyMap(
+            EI_loader.paramaters.K2, EI_loader.paramaters.d2,
+            EI_loader.paramaters.R2, EI_loader.paramaters.P2[0:3, 0:3],
+            im_size, cv2.CV_32FC1)
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        i = 0
+        while i < len(images) and not rospy.is_shutdown():
+            frame1, frame2 = ac.find_date(images, i)
+            if frame1 is not None and frame2 is not None:
+                img1 = cv2.imread(frame1)
+                img2 = cv2.imread(frame2)
+
+                # Publish messages
+                cam_msg1 = construct_info_message(
+                    im_size, EI_loader.paramaters.K1, EI_loader.paramaters.d1,
+                    EI_loader.paramaters.R1, EI_loader.paramaters.P1)
+                cam_msg2 = construct_info_message(
+                    im_size, EI_loader.paramaters.K2, EI_loader.paramaters.d2,
+                    EI_loader.paramaters.R2, EI_loader.paramaters.P2)
+
+                header1 = get_header(1)
+                header2 = get_header(2)
+
+                cam_msg1.header = header1
+                cam_msg2.header = header2
+
+                info1_pub.publish(cam_msg1)
+                info2_pub.publish(cam_msg2)
+
+                rate.sleep()
+
+                try:
+                    ros_img1 = bridge.cv2_to_imgmsg(img1, "bgr8")
+                    ros_img2 = bridge.cv2_to_imgmsg(img2, "bgr8")
+                    ros_img1.header = header1
+                    ros_img2.header = header2
+                    img1_pub.publish(ros_img1)
+                    img2_pub.publish(ros_img2)
+                except CvBridgeError as e:
+                    print(e)
+            i += 1
+
+
+
+
+if __name__ == '__main__':
+    rospy.init_node("ros_manta_stream")
+    parser = argparse.ArgumentParser("stream images and info over ROS")
+    parser.add_argument("images", help="Base directory to images")
+    parser.add_argument(
+        "--calibration_yaml",
+        help="Path to calibration yaml specify path of calibration files",
+        default=dirname(dirname(abspath(
+            __file__))) + "/cfg/calibrationConfig.yaml")
+    args = parser.parse_args()  # parse the command line arguments
+
+    main(args)
--- scripts/amp_yolo_stream.py
+++ scripts/amp_yolo_stream.py
@@ -8,10 +8,15 @@ import yaml
 from time import sleep
 from os.path import dirname, abspath
 from ampProc.intrinsic_extrinsic import Loader, ExtrinsicIntrnsicLoaderSaver
+import sys
+from shapely.geometry import Polygon
 
 
-def draw_circles(img, square, radius=10, color=(255, 0, 0)):
-    for sq in square:
+def construct_polygon(squares):
+    pass
+
+def draw_circles(img, squares, radius=10, color=(255, 0, 0)):
+    for sq in squares:
         if sq is not None:
             lower_left = (sq.lower_x, sq.lower_y)
             upper_left = (sq.lower_x, sq.upper_y)
@@ -20,7 +25,6 @@ def draw_circles(img, square, radius=10, color=(255, 0, 0)):
             circles = [lower_left, upper_left, upper_right, lower_right]
 
             for circle in circles:
-                print(circle)
                 cv2.circle(img, circle, radius, color, thickness=-1)
 
 
@@ -45,50 +49,53 @@ def main(detector):
     img1 = cv2.imread(images[0][0])
     im_size = img1.shape[:2]
     EI_loader = ExtrinsicIntrnsicLoaderSaver(loader, im_size)
-    # # Get rectification maps
+    # Get rectification maps
     if EI_loader.paramaters.R1 is None or EI_loader.paramaters.R1 is None:
         EI_loader.calculate_rectification_matracies()
-    # new_k2 = np.eye(3)
-    # EI_loader.paramaters.d2[4] = 0
-    new_k2, roi = cv2.getOptimalNewCameraMatrix(
-        EI_loader.paramaters.K2, EI_loader.paramaters.d2, im_size, 1, im_size)
-    # Compute rectification maps
-    print(EI_loader.paramaters.R2.shape, new_k2.shape)
-    map1, map2 = cv2.initUndistortRectifyMap(
+
+    map1_1, map1_2 = cv2.initUndistortRectifyMap(
+            EI_loader.paramaters.K1, EI_loader.paramaters.d1,
+            EI_loader.paramaters.R1, EI_loader.paramaters.P1[0:3, 0:3],
+            im_size, cv2.CV_32FC1)
+    map2_1, map2_2 = cv2.initUndistortRectifyMap(
             EI_loader.paramaters.K2, EI_loader.paramaters.d2,
-            EI_loader.paramaters.R2, new_k2, im_size, cv2.CV_32FC1)
+            EI_loader.paramaters.R2, EI_loader.paramaters.P2[0:3, 0:3],
+            im_size, cv2.CV_32FC1)
 
     for i in range(len(images)):
         frame1, frame2 = ac.find_date(images, i)
         if frame1 is not None and frame2 is not None:
             img1 = cv2.imread(frame1)
             img2 = cv2.imread(frame2)
-            print(EI_loader.paramaters.d1)
-            print(EI_loader.paramaters.d2)
+            # undistort images
             img1 = cv2.undistort(
                 img1, EI_loader.paramaters.K1, EI_loader.paramaters.d1)
             img2 = cv2.undistort(
-                img2, EI_loader.paramaters.K2, EI_loader.paramaters.d2)
-            # rectify image 2
-            img2 = cv2.remap(img2, map1, map2, cv2.INTER_LINEAR)
+                    img2, EI_loader.paramaters.K2, EI_loader.paramaters.d2)
+            # rectify images
+            img1 = cv2.remap(img1, map1_1, map1_2, cv2.INTER_LINEAR)
+            img2 = cv2.remap(img2, map2_1, map2_2, cv2.INTER_LINEAR)
 
             if img1 is None or img2 is None:
                 continue
 
-            detection1, sq1 = detector.stream_img(
+            detection1, squares1 = detector.stream_img(
                             img1, frame1.split('/')[-1],
                             display_name_append="1")
-            detection2, sq2 = detector.stream_img(
+            detection2, squares2 = detector.stream_img(
                             img2, frame2.split('/')[-1],
                             display_name_append="2")
 
             if detection1 and detection2:
-                draw_circles(img1, sq1)
-                draw_circles(img2, sq2)
-                draw_circles(img1, sq2, color=(0, 0, 255))
+                draw_circles(img1, squares1)
+                draw_circles(img2, squares2)
+                draw_circles(img1, squares2, color=(0, 0, 255))
                 cv2.imshow("frame1", img1)
                 cv2.imshow("frame2", img2)
-                cv2.waitKey(0)
+                k = cv2.waitKey(0)
+                if k == 113:  # q
+                    cv2.destroyAllWindows()
+                    sys.exit()
 
             sleep(pause)
 
