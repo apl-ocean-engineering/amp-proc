5cdce50419f31c0f6434bae6d18d59e94dbc9c9c
Mitchell Scott <miscott@uw.edu>
2019-09-25 17:06:10 -0700

    Updating rectified image search for similar objects in both Manta cameras

--- scripts/amp_yolo_stream.py
+++ scripts/amp_yolo_stream.py
@@ -1,31 +1,79 @@
 #!/usr/bin/env python3
+
+"""
+@author: Mitchell Scott
+@contact: miscott@uw.edu
+"""
+
 from pytorchYolo.detector import YoloLiveVideoStream
 from pytorchYolo.argLoader import ArgLoader
 from ampProc.amp_common import ampCommon, CvSquare
 import cv2
+
 import glob
 import yaml
-from time import sleep
 from os.path import dirname, abspath
 from ampProc.intrinsic_extrinsic import Loader, ExtrinsicIntrnsicLoaderSaver
 import sys
 from shapely.geometry import Polygon
+import copy
 import time
+import signal
 
 x_motion = 1
 
+name1 = "img1"
+name2 = "img2"
 
-def test_square_overlap(sq2, square1_list, img=None):
-    # draw_circles(img, sq2)
-    # cv2.imshow("frame1", img)
-    # cv2.waitKey(1)
+# cv2.namedWindow(name1, cv2.WINDOW_NORMAL)
+# cv2.namedWindow(name2, cv2.WINDOW_NORMAL)
+
+overlap_threshold = 0.4
+
+min_det_objs = 6
+min_shared_objs = 6
+
+sq_x_extend = 0
+sq_y_extend = 50
+
+
+def sigint_handler(signum, frame):
+    """
+    Exit system if SIGINT
+    """
+    sys.exit()
+
+
+def parse_waitKey(k):
+    if k == 113:  # q
+        sys.exit()
+
+
+def draw_images(img1, img2, name1=name1, name2=name2, wait=0):
+    cv2.imshow(name1, img1)
+    cv2.imshow(name2, img2)
+
+    k = cv2.waitKey(wait)
+    parse_waitKey(k)
+
+
+def test_square_overlap(sq2, square1_list, img1=None, img2=None):
+    # draw_circles(img1, sq2, color=(0,0,255))
     for sq1 in square1_list:
+        # draw_circles(img1, sq1)
+        # draw_images(img1, img2, wait=1)
         p1 = construct_polygon(sq1)
         p2 = construct_polygon(sq2)
-        if p1.intersects(p2):
-            return True
 
-    return False
+        if p1.intersects(p2):
+
+            max_area = max(p1.area, p2.area)
+            overlap_over_area = p1.intersection(p2).area/max_area
+            # print(overlap_over_area)
+            if overlap_over_area > overlap_threshold:
+                return True, sq1
+
+    return False, None
 
 
 def deconstruct_square(sq):
@@ -56,38 +104,104 @@ def build_squares(im_size, yolo_squares):
         if ysq is not None:
             sq = CvSquare(im_size)
             sq.set_from_YOLO_square(ysq)
+            sq.extend_square(sq_x_extend, sq_y_extend)
             squares.append(sq)
 
     return squares
 
 
-def check_detection(squares1, squares2, img1=None, img2=None):
-    # if img1 is not None:
-        # for sq1 in squares1:
-        #     draw_circles(img1, sq1)
-    time_init = time.time()
-    for sq2 in squares2:
-        valid = sq2.move_square()
-        while valid:
-            overlap = test_square_overlap(sq2, squares1, img1)
-            if overlap:
-                print("OVERLAP")
-                cv2.waitKey(0)
-                break
-            valid = sq2.move_square()
-        # if img1 is not None:
-        #     draw_circles(img1, sq2, color=(0, 0, 255))
-    print(time.time() - time_init)
+def get_detection_squares(detector, img, frame, display_name_append="1"):
+    detection, yolo_squares = detector.stream_img(
+                    img, frame.split('/')[-1],
+                    display_name_append=display_name_append)
 
-def main(detector):
+    squares = build_squares(img.shape[0:2], yolo_squares)
+
+    return detection, squares
+
+
+def check_stereo_detection(squares1, squares2, img1=None, img2=None,
+                                        show_YOLO=False, args=None):
+    for sq2 in squares2:
+        time_init = time.time()
+        sq2_init = copy.deepcopy(sq2)
+        # draw_circles(img2, sq2)
+        # Search forward
+        valid = sq2.move_square(motion=1)
+        while valid:
+            overlap, sq1 = test_square_overlap(
+                    sq2, squares1,
+                    img1=img1, img2=img2)
+            if overlap:
+                if show_YOLO:
+                    if args is not None:
+                        draw_circles(img1, sq1)
+                        draw_circles(img2, sq2_init)
+                        draw_images(img1, img2, wait=0)
+                    else:
+                        print("NO ARGS PASSED")
+                return True
+            valid = sq2.move_square()
+
+        # Search forward
+        valid = sq2.move_square(motion=-1)
+        while valid:
+            overlap, sq1 = test_square_overlap(
+                    sq2, squares1,
+                    img1=img1, img2=img2)
+            if overlap:
+                if show_YOLO:
+                    if args is not None:
+                        draw_circles(img1, sq1)
+                        draw_circles(img2, sq2_init)
+                        draw_images(img1, img2, wait=0)
+                    else:
+                        print("NO ARGS PASSED")
+                return True
+            valid = sq2.move_square()
+
+        print("time elapsed", time.time() - time_init)
+
+    return False
+
+
+def load_undistort_rectify_image(fname, K1, d1, map1, map2):
+    img = cv2.imread(fname)
+    raw_img = copy.deepcopy(img)
+
+    # undistort images
+    img = cv2.undistort(img, K1, d1)
+
+    # rectify images
+    img = cv2.remap(img, map1, map2, cv2.INTER_LINEAR)
+
+    return img, raw_img
+
+
+def detection(detection1, detection2, squares1, squares2, img1, img2):
+
+    if len(squares1) > min_det_objs or len(squares2) > min_det_objs:
+        return True
+
+    if detection1 and detection2:
+        if len(squares1) > min_shared_objs and len(squares2) > min_shared_objs:
+            return True
+
+        stereo_detection = check_stereo_detection(
+            squares1, squares2, img1=img1, img2=img2,
+            show_YOLO=False, args=args)
+
+        if stereo_detection:
+            return True
+
+
+def main(args, detector):
     with open(args.calibration_yaml, 'r') as stream:
         calibration_loader = yaml.safe_load(stream)
 
     loader = Loader()
     loader.load_params_from_file(calibration_loader)
 
-    cv2.namedWindow('frame1', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('frame2', cv2.WINDOW_NORMAL)
     manta1 = sorted(glob.glob(args.images + "/Manta1/*.jpg"))
     manta2 = sorted(glob.glob(args.images + "/Manta2/*.jpg"))
 
@@ -100,56 +214,61 @@ def main(detector):
     img1 = cv2.imread(images[0][0])
     im_size = img1.shape[:2]
     EI_loader = ExtrinsicIntrnsicLoaderSaver(loader, im_size)
+
     # Get rectification maps
     if EI_loader.paramaters.R1 is None or EI_loader.paramaters.R1 is None:
         EI_loader.calculate_rectification_matracies()
 
+    # Load rectification maps
     map1_1, map1_2 = cv2.initUndistortRectifyMap(
             EI_loader.paramaters.K1, EI_loader.paramaters.d1,
             EI_loader.paramaters.R1, EI_loader.paramaters.P1[0:3, 0:3],
             im_size, cv2.CV_32FC1)
+
     map2_1, map2_2 = cv2.initUndistortRectifyMap(
             EI_loader.paramaters.K2, EI_loader.paramaters.d2,
             EI_loader.paramaters.R2, EI_loader.paramaters.P2[0:3, 0:3],
             im_size, cv2.CV_32FC1)
 
     for i in range(len(images)):
-        frame1, frame2 = ac.find_date(images, i)
-        if frame1 is not None and frame2 is not None:
-            img1 = cv2.imread(frame1)
-            img2 = cv2.imread(frame2)
-            # undistort images
-            img1 = cv2.undistort(
-                img1, EI_loader.paramaters.K1, EI_loader.paramaters.d1)
-            img2 = cv2.undistort(
-                    img2, EI_loader.paramaters.K2, EI_loader.paramaters.d2)
-            # rectify images
-            img1 = cv2.remap(img1, map1_1, map1_2, cv2.INTER_LINEAR)
-            img2 = cv2.remap(img2, map2_1, map2_2, cv2.INTER_LINEAR)
+        signal.signal(signal.SIGINT, sigint_handler)
+        try:
+            frame1, frame2 = ac.find_date(images, i)
+            if frame1 is not None and frame2 is not None:
+                img1, raw_img1 = load_undistort_rectify_image(
+                    frame1, EI_loader.paramaters.K1, EI_loader.paramaters.d1,
+                    map1_1, map1_2)
 
-            if img1 is None or img2 is None:
-                continue
+                img2, raw_img2 = load_undistort_rectify_image(
+                    frame2, EI_loader.paramaters.K2, EI_loader.paramaters.d2,
+                    map2_1, map2_2)
 
-            detection1, yolo_squares1 = detector.stream_img(
-                            img1, frame1.split('/')[-1],
-                            display_name_append="1")
-            detection2, yolo_squares2 = detector.stream_img(
-                            img2, frame2.split('/')[-1],
-                            display_name_append="2")
+                if img1 is None or img2 is None:
+                    continue
 
-            squares1 = build_squares(im_size, yolo_squares1)
-            squares2 = build_squares(im_size, yolo_squares2)
+                # draw_images(raw_img1, raw_img2, wait=1)
 
-            if detection1 and detection2:
-                check_detection(squares1, squares2, img1=img1, img2=img2)
-                cv2.imshow("frame1", img1)
-                cv2.imshow("frame2", img2)
-                k = cv2.waitKey(1)
-                if k == 113:  # q
-                    cv2.destroyAllWindows()
-                    sys.exit()
+                detection1, squares1 = get_detection_squares(
+                        detector, img1, frame1.split('/')[-1],
+                        display_name_append="1")
 
-            sleep(pause)
+                detection2, squares2 = get_detection_squares(
+                        detector, img2, frame2.split('/')[-1],
+                        display_name_append="2")
+
+                obj_found = detection(
+                    detection1, detection2, squares1, squares2, img1, img2)
+
+                if obj_found:
+                    print("DETECTED")
+                    print(frame1)
+                    # pass
+                    # draw_images(raw_img1, raw_img2, wait=0)
+                else:
+                    pass
+                    # draw_images(raw_img1, raw_img2, wait=1)
+        except:
+            pass
 
 
 if __name__ == '__main__':
@@ -159,9 +278,15 @@ if __name__ == '__main__':
         help="Path to calibration yaml specify path of calibration files",
         default=dirname(dirname(abspath(
             __file__))) + "/cfg/calibrationConfig.yaml")
+    argLoader.parser.add_argument(
+        "--save_path",
+        help="Path to save positive detections",
+        default=dirname(dirname(abspath(
+            __file__))) + "/data/detections.txt")
     args = argLoader.args  # parse the command line arguments
     pause = 0.01
 
     detector = YoloLiveVideoStream(args)
+    detector.display = False
 
-    main(detector)
+    main(args, detector)
